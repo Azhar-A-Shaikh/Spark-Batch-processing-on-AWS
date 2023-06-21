13dc33a6e9b2746fa09d0c0b627db758083f524f
Azhar-A-Shaikh <117568791+Azhar-A-Shaikh@users.noreply.github.com>
2023-06-21 12:11:39 +0530

    Update check_processing.py

--- check_processing.py
+++ check_processing.py
@@ -5,11 +5,11 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("souce process").getOrCreate()
 
-source_df = spark.read.csv("Replace your S3 location here", header=True)
+source_df = spark.read.csv("s3://pysparkapi/banktxn/csv/banktxn.csv", header=True)
 
 # to write into parquet format. 
  
-source_df.write.parquet("Replace your S3 location here")
+source_df.write.parquet("s3://pysparkapi/banktxn/parquet")
 
 
 
