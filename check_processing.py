70b5502005dc9e1b7ee47a5a4752e31a9414ca77
Azhar-A-Shaikh <117568791+Azhar-A-Shaikh@users.noreply.github.com>
2023-06-26 17:03:24 +0530

    Update check_processing.py

--- check_processing.py
+++ check_processing.py
@@ -5,11 +5,11 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("souce process").getOrCreate()
 
-source_df = spark.read.csv("s3://pysparkapi/banktxn/csv/banktxn.csv", header=True)
+source_df = spark.read.csv("s3://py-sparkapi/banktxn/csv/banktxn.csv", header=True)
 
 # to write into parquet format. 
  
-source_df.write.parquet("s3://pysparkapi/banktxn/parquet") 
+source_df.write.parquet("s3://py-sparkapi/banktxn/parquet") 
 
 
 
